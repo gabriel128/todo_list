6e43ca504d95f6a3bafb9123081f01cb391de7c4
Gabriel Torello <gtorello@starmount.com>
2014-02-14 16:05:52 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectGabriel::Application.config.secret_key_base = 'f3ef149931206d6cba5e178ee6900a5bf09ff6092406ebd4503585d21d8cbe1228419a21f6aea65ac9991d8bd4497764ff6907a49fb1c199edf1bfc0df6ec155'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectGabriel::Application.config.session_store :cookie_store, key: '_Project-Gabriel_session'
