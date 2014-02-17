3df1a9c4328089889a399a15e147f4ae0bbad895
Gabriel Torello <gtorello@starmount.com>
2014-02-17 10:27:33 -0300

    for merge

---
+++ spec/model/todo_list_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'Todo List' do
+
+  it 'has to be true' do
+    expect(true).to be_true
+  end
+
+  it 'is automatically saved when an entry is entered'
+
+  it 'does not have any entry when emptied by empty entries'
+end
